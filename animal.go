885997eadecaa4be966ec9aa6f04722a509caeca
Patrick Kim <patrickskim@me.com>
2015-04-02 01:58:31 -0400

    ... wtf

--- animal.go
+++ animal.go
@@ -1,17 +1,20 @@
 package animal
 
 // Animal represents a class?
-type Animal struct {
-	age     int
-	name    string
-	species string
+type Animal interface {
+	Talk() string
 }
 
-func (d *Animal) talk() (sound string) {
-	if d.species == "dog" {
-		sound = "woof"
-	} else {
-		sound = "yellp"
-	}
-	return
-}
+//
+// age     int
+// name    string
+// species string
+//
+// func (d *Animal) talk() (sound string) {
+// 	if d.species == "dog" {
+// 		sound = "woof"
+// 	} else {
+// 		sound = "yellp"
+// 	}
+// 	return
+// }
