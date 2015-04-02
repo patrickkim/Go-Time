1374926b5c198d44641bd16b26ac9993a07de73b
Patrick Kim <patrickskim@me.com>
2015-04-02 02:08:17 -0400

    maybe i do know what im doing

--- hello.go
+++ hello.go
@@ -10,7 +10,23 @@ func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Printf("That file is %s. \n", humanize.Bytes(82854982))
 
-	// spot := Animal{name: "spot", age: 21, species: "dog"}
-	// spot.name = "wally"
-	// fmt.Printf("me %s , me can %v \n", spot.name, spot.talk())
+	spot := Animal{name: "spot", age: 21, species: "thing"}
+	spot.name = "wally"
+	fmt.Printf("me %s , me can %v \n", spot.name, spot.talk())
+}
+
+// Animal represents a class?
+type Animal struct {
+	age     int
+	name    string
+	species string
+}
+
+func (animal *Animal) talk() (sound string) {
+	if animal.species == "dog" {
+		sound = "woof"
+	} else {
+		sound = "yellp"
+	}
+	return
 }
